bd10bd8e7014735d8a354e49fd0d606ed0aebbfc
Toma Vladimirskaya <vladimirskayatamara@gmail.com>
2022-08-30 14:40:11 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./js/jquery-input-mask-phone-number.js"
+import "./js/common.js";
+import "./assets/css/main.css";
+import "./assets/scss/main.scss";
+
---
+++ src/js/common.js
@@ -0,0 +1,161 @@
+$(".condition__showmore").on("click", function () {
+  $(".condition__description").toggleClass('active');
+})
+
+
+
+$(document).ready(function(){
+    $('#form__input-phone').inputmask('+7 (999) 999-99-99')
+});
+
+
+// Инициализируем Swiper
+let myImageSlider = new Swiper('.condition__items', {
+	// Включение/отключение
+	// перетаскивания на ПК
+	simulateTouch: true,
+	// Чувствительность свайпа
+	touchRatio: 1,
+	// Угол срабатывания свайпа/перетаскивания
+	touchAngle: 45,
+	// Курсор перетаскивания
+	grabCursor: true,
+
+	// Переключение при клике на слайд
+	slideToClickedSlide: false,
+
+	// Навигация по хешу
+	hashNavigation: {
+		// Отслеживать состояние
+		watchState: true,
+	},
+
+	// Управление клавиатурой
+	keyboard: {
+		// Включить\выключить
+		enabled: true,
+		// Включить\выключить
+		// только когда слайдер
+		// в пределах вьюпорта
+		onlyInViewport: true,
+		// Включить\выключить
+		// управление клавишами
+		// pageUp, pageDown
+		pageUpDown: true,
+	},
+
+	// Управление колесом мыши
+	mousewheel: {
+		// Чувствительность колеса мыши
+		sensitivity: 1,
+	},
+
+	// Автовысота
+	autoHeight: false,
+
+	// Количество слайдов для показа
+	slidesPerView: 3,
+
+	// Отключение функционала
+	// если слайдов меньше чем нужно
+	watchOverflow: true,
+
+	// Отступ между слайдами
+	spaceBetween: 30,
+
+	// Количество пролистываемых слайдов
+	slidesPerGroup: 1,
+
+	// Активный слайд по центру
+	centeredSlides: false,
+
+	// Стартовый слайд.
+	initialSlide: 0,
+
+	// Мультирядность
+	slidesPerColumn: 1,
+
+	// Бесконечный слайдер
+	loop: false,
+
+	// Кол-во дублирующих слайдов
+	loopedSlides: 0,
+
+	// Свободный режим
+	freeMode: true,
+
+	// Скорость
+	speed: 800,
+
+	// Вертикальный слайдер
+	direction: 'horizontal',
+
+
+	// Эффекты переключения слайдов.
+	// Листание
+	effect: 'slide',
+
+	// Отключить предзагрузка картинок
+	preloadImages: false,
+	// Lazy Loading
+	// (подгрузка картинок)
+	lazy: {
+		// Подгружать на старте
+		// переключения слайда
+		loadOnTransitionStart: false,
+		// Подгрузить предыдущую
+		// и следующую картинки
+		loadPrevNext: false,
+	},
+	// Слежка за видимыми слайдами
+	watchSlidesProgress: true,
+	// Добавление класса видимым слайдам
+	watchSlidesVisibility: true,
+
+	// Зум картинки
+	zoom: {
+		// Макимальное увеличение
+		maxRatio: 5,
+		// Минимальное увеличение
+		minRatio: 1,
+	},
+
+	// Обновить свайпер
+	// при изменении элементов слайдера
+	observer: true,
+
+	// Обновить свайпер
+	// при изменении родительских
+	// элементов слайдера
+	observeParents: true,
+
+	// Обновить свайпер
+	// при изменении дочерних
+	// элементов слайда
+	observeSlideChildren: true,
+
+});
+
+
+let swiper = new Swiper('.gallery__slider', {
+  slidesPerView: 'auto',
+  scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: false,
+      draggable: true,
+      snapOnRelease: false,
+  },
+  pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+  },
+  navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+  },
+});
+
+
+
+
+
